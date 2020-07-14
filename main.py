8477923124111406a88dba5d3a592670180367a5
Concrete18 <michaelericson19@gmail.com>
2020-07-14 18:43:01 -0400

    small changes

---
+++ main.py
@@ -0,0 +1,15 @@
+import shutil
+
+def main():
+    backup_storage = ''
+    documents_directory = ''
+
+    def backup_save():
+        print('test')
+
+    def backup_save():
+        print('test')
+
+
+if __name__ == '__main__':
+    main()
