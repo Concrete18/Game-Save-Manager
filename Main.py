97f78b5eb4613c9eb67278e6e429ac06a6af1648
Concrete18 <michaelericson19@gmail.com>
2020-07-22 16:28:43 -0400

    Fixed basic backup use of interface

--- Main.py
+++ Main.py
@@ -14,11 +14,8 @@ def main():
     save_location text,
     last_backup text
     )''')
-    script_root = os.getcwd()
     backup_redundancy = 3 # Total previous backups to keep after each backup is made.
-    backup_storage = 'placeholder'
-    testing_storage = os.path.join(os.getcwd(), 'Testing Area\\Save Backup')
-    os.chdir(testing_storage)
+    backup_storage = 'Testing Area\\Save Backup'
 
 
     def Add_Game_to_DB():
@@ -31,7 +28,7 @@ def main():
 
 
     def get_save_loc(game):
-        print(game)
+        print(f'Getting Save location for {game}.')
         game_list = sqlite3.connect('game_list.db')
         c = game_list.cursor()
         c.execute("SELECT save_location FROM games WHERE game_name=:game_name", {'game_name': game})
@@ -51,12 +48,11 @@ def main():
 
     def Delete_Oldest(game):
         saves_list = []
-        dir = os.path.join(os.getcwd(), game)
+        dir = os.path.join(backup_storage, game)
+        print(dir)
         for file in os.listdir(dir):
-            print(file)
             file = os.path.join(dir, file)
             saves_list.append(file)
-        print(saves_list)
         if len(saves_list) < 4:
             print('3 or Less Saves.')
             return
@@ -68,8 +64,7 @@ def main():
 
     def Save_Backup(game):
         current_time = dt.datetime.now().strftime("%d-%m-%y %H-%M")
-        dest = f'{os.getcwd()}\\{game}\\{current_time}'
-        dest = os.path.join(os.getcwd(), game, current_time)
+        dest = os.path.join(backup_storage, game, current_time)
         save_loc = get_save_loc(game)
         try:
             shutil.copytree(save_loc, dest)
@@ -77,7 +72,10 @@ def main():
             print('No Action Completed - File location does not exist.')
         except FileExistsError:
             print('No Action Completed - Save Already Backed up.')
-        game_list.at[game, 'Last Backup'] = dt.datetime.now()
+        c = game_list.cursor()
+        var = dt.datetime.now()
+        c.execute("""UPDATE games SET last_backup = :last_backup WHERE game_name = :game_name""",
+        {'game_name': game, 'last_backup': var})
         Delete_Oldest(game)
 
 
@@ -98,29 +96,23 @@ def main():
     Title = Tk.Label(Title_Frame, text='Game Save Manager', font=(BoldBaseFont, 20), bg=Background)
     Title.grid(column=0, row=0)
 
-    # Create a Tkinter variable
-    tkvar = Tk.StringVar(Main_GUI)
-    sorted_list = Game_list_Sorted()
+    def Clicked_Backup():
+        Save_Backup(clicked.get())
 
-    popupMenu = Tk.OptionMenu(Main_GUI, tkvar, *sorted_list)
+    clicked = Tk.StringVar(Main_GUI)
+    sorted_list = Game_list_Sorted()
+    clicked.set(sorted_list[0]) # set the default option
+
+    popupMenu = Tk.OptionMenu(Main_GUI, clicked, *sorted_list)
     popupMenu.grid(row=2, column=1)
     popupMenu["menu"].config(bg=Background)
-    tkvar.set(sorted_list[0]) # set the default option
-    # popupMenu.focus_set()
 
-    # on change dropdown value
-    def change_dropdown(*args):
-        print( tkvar.get() )
-
-    # link function to change dropdown
-    tkvar.trace('w', change_dropdown)
-
-    BackupButton = Tk.Button(text='Backup', command=partial(Save_Backup, tkvar.get()))
+    BackupButton = Tk.Button(text='Backup', command=partial(Clicked_Backup))
     BackupButton.grid(row=2, column=2)
 
     Main_GUI.mainloop()
+    game_list.close()
 
 
 if __name__ == '__main__':
     main()
-    game_list.close()
