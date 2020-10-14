ef9514524c3f9cc1887cc16f3d2e3b12ac4f9a42
Concrete18 <michaelericson19@gmail.com>
2020-10-14 15:24:42 -0400

    finished Restore function

--- Backup_Class.py
+++ Backup_Class.py
@@ -3,9 +3,7 @@ from tkinter import ttk
 import tkinter as Tk
 import datetime as dt
 import logging as lg
-import sqlite3
 import shutil
-import time
 import math
 import json
 import os
@@ -40,9 +38,11 @@ class Backup:
         if missing_saves > 0 and missing_saves < 6:
             msg = f'Save Locations for the following do not exist.\n{missing_save_loc}'
             continue_var = Tk.messagebox.showwarning(title='Game Save Manager', message=msg)
+            self.logger.debug(f'Missing Save Locations:{missing_save_loc}')
         elif len(missing_save_loc) > 5:
             msg = 'More than 5 save locations do not exist.'
             continue_var = Tk.messagebox.showwarning(title='Game Save Manager', message=msg)
+            self.logger.debug(f'More then 4 save locations in the database do not exist.')
 
 
     def Sanitize_For_Filename(self, string):
@@ -54,7 +54,6 @@ class Backup:
 
     def Get_Save_Loc(self, game):
         '''Returns the save location of the entered game from the SQLite Database.'''
-        print(f'Getting Save location for {game}.')
         c = self.database.cursor()
         c.execute("SELECT save_location FROM games WHERE game_name=:game_name", {'game_name': game})
         save_location = c.fetchone()[0]
@@ -76,18 +75,18 @@ class Backup:
         '''Deletest the oldest saves so only the the newest specified ammount is left.'''
         saves_list = []
         dir = os.path.join(self.backup_dest, game)
-        print(dir)
         for file in os.listdir(dir):
             file = os.path.join(dir, file)
             saves_list.append(file)
         if len(saves_list) < 4:
-            print(f'{self.backup_redundancy} or Less Saves.')
+            self.logger.info(f'{game} has {self.backup_redundancy} or Less Saves.')
             return
         else:
-            print(f'More than {self.backup_redundancy} Saves.')
             sorted_list = sorted(saves_list, key=os.path.getctime, reverse=True)
             for i in range(self.backup_redundancy, len(saves_list)):
                 shutil.rmtree(sorted_list[i])
+            self.logger.info(f'{game} had more then {self.backup_redundancy} Saves. Deleted oldest saves.')
+
 
     def Save_Backup(self, game, info_label):
         '''Backups up the game entered based on SQLite save location data to the specified backup folder.'''
@@ -107,12 +106,13 @@ class Backup:
         c.execute("""UPDATE games SET last_backup = :last_backup WHERE game_name = :game_name""",
         {'game_name': game, 'last_backup': dt.datetime.now()})
         self.database.commit()
-        self.logger.debug(f'Backed-up Save for {game}.')
+        self.logger.info(f'Backed-up Save for {game}.')
 
 
     def Restore_Backup(self):
         '''Restores game save after moving current save to special backup folder.'''
         backup_list =[]
+        selected_game = self.selected_game
         backup_path = os.path.join(self.backup_dest, self.selected_game)
         save_location = self.Get_Save_Loc(self.selected_game)
         print(save_location)
@@ -126,12 +126,20 @@ class Backup:
 
 
         def Restore_Game_Pressed():
-            print('Pressed Restore')
             save_name = save_listbox.get(save_listbox.curselection())
-            save_path = os.path.join(self.backup_dest, self.selected_game, save_name)
-            print(save_path)
-            # shutil.copytree(source, save_loc)
-            # logger.debug(f'Restored Save for {self.selected_game}.')
+            save_path = os.path.join(self.backup_dest, selected_game, save_name)
+            if os.path.exists(f'{save_location}.old'):
+                msg = '''Backup of current save before last restore already exists.
+                    \nDo you want to delete it? This will cancel the restore.'''
+                response = Tk.messagebox.askyesno(title='Game Save Manager', message=msg)
+                if response in ['yes', 'y']:
+                    os.removedirs(f'{save_location}.old')
+                else:
+                    print('Canceling Restore.')
+                    return
+            os.rename(save_location, f'{save_location}.old')
+            shutil.copytree(save_path, save_location)
+            self.logger.info(f'Restored Save for {selected_game}.')
             Restore_Game_Window.destroy()
 
 
@@ -162,6 +170,7 @@ class Backup:
         CancelButton.grid(row=2, column=1, padx=5, pady= 5)
 
         Restore_Game_Window.mainloop()
+
 
     def Convert_Size(self):
         '''Converts size of directory best fitting '''
@@ -198,7 +207,7 @@ class Backup:
         c.execute("INSERT INTO games VALUES (:game_name, :save_location, :last_backup)",
         {'game_name': game, 'save_location': save_location, 'last_backup': dt.datetime.now()})
         self.database.commit()
-        self.logger.debug(f'Added {game} to database.')
+        self.logger.info(f'Added {game} to database.')
 
 
     def Delete_Game_from_DB(self, Listbox):
@@ -211,7 +220,7 @@ class Backup:
             self.database.commit()
             selected_game = Listbox.curselection()
             Listbox.delete(selected_game[0])
-            self.logger.debug(f'Deleted {self.selected_game} from database.')
+            self.logger.info(f'Deleted {self.selected_game} from database.')
 
 
     def Update_Game(self, GameNameEntry, GameSaveEntry, Listbox):
@@ -228,6 +237,7 @@ class Backup:
             data = (game_name, save_location, self.selected_game)
             c.execute(sql_update_query , data)
             self.database.commit()
+            self.logger.info(f'Updated {self.selected_game} in database.')
         else:
             msg = f'Save Location does not exist.'
             continue_var = Tk.messagebox.showwarning(title='Game Save Manager', message=msg)
