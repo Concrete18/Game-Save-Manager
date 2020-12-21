05aaabd797a7d4664b548aab3c266f6c77e1455a
Concrete18 <michaelericson19@gmail.com>
2020-12-21 00:43:46 -0500

    Fixed unit tests

--- testing.py
+++ testing.py
@@ -1,27 +1,26 @@
-from unittest.mock import patch
 from GSM import Backup
 import unittest
 import os
 
 
-class TestAFC(unittest.TestCase):
+class TestGameSaveManager(unittest.TestCase):
 
 
-    def test_Sanitize_For_Filename(self):
+    def test_selected_game_filename(self):
         App = Backup()
         tests = {
         'Amnesia: The Dark Descent':'Amnesia The Dark Descent',
         'Is&this<>correct?':'Isthiscorrect',
         'This is a long test of the file sanitize function':'This is a long test of the file'
         }
-        for test, answer in tests.items():
-            self.assertEqual(App.Sanitize_For_Filename(test), answer)
+        for test_value, answer in tests.items():
+            self.assertEqual(App.selected_game_filename(test_value), answer)
 
 
-    def test_Convert_Size(self):
+    def test_convert_size(self):
         App = Backup()
-        self.assertEqual(App.Convert_Size(os.path.join(os.getcwd(), 'Testing\Folder Example')), '124.0 B')
-        self.assertEqual(App.Convert_Size('ValueError'), '0 bits')
+        self.assertEqual(App.convert_size(os.path.join(os.getcwd(), 'Testing\Folder Example')), '124.0 B')
+        self.assertEqual(App.convert_size('ValueError'), '0 bits')
 
 
 if __name__ == '__main__':
