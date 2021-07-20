7aa61792da94fc82bbe74eb67e94e654eaddb516
Concrete18 <michaelericson19@gmail.com>
2021-07-20 00:05:27 -0400

    fixed testing after game class addition

--- testing.py
+++ testing.py
@@ -1,28 +1,30 @@
-from game_save_manager import Backup_Class
+from game_save_manager import Backup_Class, Game
 from time import sleep, perf_counter
 import unittest
 import os
 
 class TestGameSaveManager(unittest.TestCase):
 
-    test = Backup_Class()
+    main = Backup_Class()
+    game = Game('Save Backup')
+    game.name = 'Test'
 
 
-    def test_selected_game_filename(self):
-        print('\nTesting selected_game_filename function')
+    def test_get_filename(self):
+        print('\nTesting get_filename function')
         tests = {
         'Amnesia: The Dark Descent':'Amnesia The Dark Descent',
         'Is&this<>correct?':'Isthiscorrect',
         '  This       is  a *&^%^ space *(&^test    ':'This is a space test',
         }
         for test_value, answer in tests.items():
-            self.assertEqual(self.test.get_selected_game_filename(test_value), answer)
+            self.assertEqual(self.game.get_filename(test_value), answer)
 
 
     def test_convert_size(self):
         print('\nTesting convert_size function')
-        self.assertEqual(self.test.convert_size(os.path.join(os.getcwd(), 'Folder Test\Folder Example')), '124.0 B')
-        self.assertEqual(self.test.convert_size('ValueError'), '0 bits')
+        self.assertEqual(self.game.convert_size(os.path.join(os.getcwd(), 'Folder Test\Folder Example')), '124.0 B')
+        self.assertEqual(self.game.convert_size('ValueError'), '0 bits')
 
 
     def test_compressed(self):
@@ -37,7 +39,7 @@ class TestGameSaveManager(unittest.TestCase):
         'Post-Restore Save.xztar':True,
         }
         for test_value, answer in tests.items():
-            self.assertEqual(self.test.compressed(test_value), answer)
+            self.assertEqual(self.main.compressed(test_value), answer)
 
 
     def test_get_appid(self):
@@ -48,7 +50,7 @@ class TestGameSaveManager(unittest.TestCase):
         'Monster Hunter: World': 582010
         }
         for test_value, answer in tests.items():
-            self.assertEqual(self.test.get_appid(test_value), answer)
+            self.assertEqual(self.main.get_appid(test_value), answer)
             sleep(.5)
 
 
@@ -75,16 +77,16 @@ class TestGameSaveManager(unittest.TestCase):
             'Breathedge':r'C:\Program Files (x86)\Steam\steamapps\common\Breathedge',
             'Timberborn':r'D:\My Documents\Timberborn',
             'XCOM 2 War of the Chosen':r'D:\My Documents\My Games\XCOM2 War of the Chosen'}
-        self.test.debug = 0
-        self.test.output = 0
+        self.main.debug = 0
+        self.main.output = 0
         print('\n   Setting up search directories')
-        self.test.find_search_directories()
+        self.main.find_search_directories()
         print('\n   Starting search for each game.')
         elapsed_total = 0
         for game, path in game_dict.items():
             print(f'    > {game}', end="")
             start = perf_counter()
-            self.assertEqual(self.test.game_save_location_search(game, test=1), path)
+            self.assertEqual(self.main.game_save_location_search(game, test=1), path)
             finish = perf_counter()
             elapsed_single = finish-start
             elapsed_total += elapsed_single
