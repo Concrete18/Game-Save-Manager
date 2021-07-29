73b347c7d45b2b18cdc127633de0ba07fb11d8e8
Concrete18 <michaelericson19@gmail.com>
2021-07-29 14:44:09 -0400

    started work on switching game save search to its own class

---
+++ test_save_search.py
@@ -0,0 +1,18 @@
+from classes.save_search import Save_Search
+from classes.game import Game
+import unittest
+
+class TestBackup(unittest.TestCase):
+
+
+    def test_get_appid(self):
+        '''
+        ph
+        '''
+        main = Save_Search(Game, 1)
+        path = r'C:\Users\Michael\AppData\Local\Teardown'
+        self.assertIn(main.game_save_location_search('Teardown', test=1), path)
+
+
+if __name__ == '__main__':
+    unittest.main()
