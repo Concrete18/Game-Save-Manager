d5a7f0bdf39aa71f86d10fe068494b887d5447fd
Concrete18 <michaelericson19@gmail.com>
2022-06-08 19:21:58 -0400

    added get_hash and helper.py turned into a class

--- classes/helper.py
+++ classes/helper.py
@@ -1,17 +1,28 @@
-import time
+from checksumdir import dirhash
+import time, os
 
 
-def benchmark(func):
-    """
-    Prints `func` name and its benchmark time.
-    """
+class Helper:
+    def benchmark(func):
+        """
+        Prints `func` name and its benchmark time.
+        """
 
-    def wrapped(*args, **kwargs):
-        start = time.perf_counter()
-        value = func(*args, **kwargs)
-        end = time.perf_counter()
-        elapsed = round(end - start, 2)
-        print(f"{func.__name__} Completion Time: {elapsed}")
-        return value
+        def wrapped(*args, **kwargs):
+            start = time.perf_counter()
+            value = func(*args, **kwargs)
+            end = time.perf_counter()
+            elapsed = round(end - start, 2)
+            print(f"{func.__name__} Completion Time: {elapsed}")
+            return value
 
-    return wrapped
+        return wrapped
+
+    def get_hash(self, dir_file):
+        """
+        Gets hash of the given file or folder.
+        """
+        if os.path.isdir(dir_file):
+            return dirhash(dir_file, "md5")
+        else:
+            return self.hash_file(dir_file)
